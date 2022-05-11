f2472aeb8bd6b76f88473013bb4315781929ecfa
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2022-05-12 01:09:44 +0530

    Add files via upload

---
+++ sketch_may12a/sketch_may12a.ino
@@ -0,0 +1,15 @@
+void setup() {
+
+  
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);   
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(900);
+  digitalWrite(13,LOW);
+  delay(200);
+  // put your main code here, to run repeatedly:
+
+}

66a64c34c8fc94b2f28906dc41572b8fe7d33bb6
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2022-05-18 00:21:56 +0530

    Update sketch_may12a.ino

--- sketch_may12a/sketch_may12a.ino
+++ sketch_may12a/sketch_may12a.ino
@@ -1,15 +1,57 @@
+int pin2 = 2;
+int pin3 = 3;
+int pin4 = 4;
+int pin5 = 5;
+
+
+
+
+
+
+
 void setup() {
 
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+  pinMode(pin4,OUTPUT);
+  pinMode(pin5,OUTPUT);
+
+  
   
   // put your setup code here, to run once:
-pinMode(13,OUTPUT);   
+
 }
 
+
 void loop() {
-  digitalWrite(13,HIGH);
-  delay(900);
-  digitalWrite(13,LOW);
-  delay(200);
+
+  digitalWrite(pin2,LOW);
+  digitalWrite(pin3,LOW);
+  digitalWrite(pin4,LOW);
+  digitalWrite(pin5,LOW);
+
+  delay(750);
+
+
+  
+  digitalWrite(pin2,HIGH);
+  digitalWrite(pin3,HIGH);
+  digitalWrite(pin4,HIGH);
+  digitalWrite(pin5,HIGH);
+  delay(759);
+
+  digitalWrite(pin2,LOW);
+  digitalWrite(pin3,LOW);
+  digitalWrite(pin4,LOW);
+  digitalWrite(pin5,HIGH);
+
+  digitalWrite(pin2,HIGH);
+  digitalWrite(pin3,LOW);
+  digitalWrite(pin4,LOW);
+  digitalWrite(pin5,HIGH);
+
+  
+ 
   // put your main code here, to run repeatedly:
 
 }
